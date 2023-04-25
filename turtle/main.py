010cbfc0278d5e2a003c9cfb7cd15313f13f4cf3
Pedro Cauan <92456337+Cauan87@users.noreply.github.com>
2023-04-25 10:28:34 -0300

    aula 05 de POO
    Testando a turtle

---
+++ turtle/main.py
@@ -0,0 +1,12 @@
+import turtle
+wn = turtle.Screen()
+donatelo = turtle.Turtle()
+donatelo.forward(50)
+donatelo.left(90)
+donatelo.forward(50)
+donatelo.left(90)
+donatelo.forward(50)
+donatelo.left(90)
+donatelo.forward(50)
+
+
